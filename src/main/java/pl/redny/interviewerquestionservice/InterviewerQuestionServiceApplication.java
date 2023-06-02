48ad36f3cfc575d0a7237c35d983e028f27aac78
Piotr Janicki <piotr.janicki@devapo.pl>
2023-06-02 22:48:19 +0200

    Initial commit

---
+++ src/main/java/pl/redny/interviewerquestionservice/InterviewerQuestionServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.redny.interviewerquestionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewerQuestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewerQuestionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/redny/interviewerquestionservice/InterviewerQuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.redny.interviewerquestionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewerQuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
